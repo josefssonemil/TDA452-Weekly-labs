7bcc9c6a6e5d1d9c2a7eccff92ef36beaf0a4c39
Emil Josefsson <emiljos@student.chalmers.se>
2018-11-07 12:19:36 +0100

    finished last steps for lab1

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -1,3 +1,5 @@
+import Test.QuickCheck
+
 -- Part 1
 
 power :: Integer -> Integer -> Integer
@@ -15,23 +17,52 @@ power n k = n * power n (k-1)
 
 -- Part 2
 -- Uses the function product on a list containing n k times. Throws error if 
--- k is negative and will only accept k as an int.
-power1 :: Double -> Int -> Double
+-- k is negative
+
+power1 :: Integer -> Integer -> Integer
 power1 n k | k < 0 = error "power: negative argument"
 power1 n 0 = 1
-power1 n k = product (take k (repeat n))
+power1 n k = product (replicate (fromIntegral k) n)
 
 
 -- Part 3
--- Integer instead of Int because we are not using the function product
--- n can be anything, but k has to be an Integer
--- Uses if/then/else to get a one liner
+
+-- Uses if/then/else to get a one liner as well as an 'even' check to know
+-- which of the below equations to use.
 --n^k = (n^2)^k/2 (k even)
 --n^k = n * (n^k-1) (k odd)
-power2 :: Double -> Integer -> Double 
+
+power2 :: Integer -> Integer -> Integer 
 power2 n k | k < 0 = error "power: negative argument"
 power2 n 0  = 1
 power2 n k = if even k then power2 (n*n) (div k 2) 
                             else (n * (power2 n (k-1)))    
 
 -- Part 4
+
+-- A
+-- Test cases: k = 0, n = 0 & k = Integer, n = 0 & k = 0, n = integer & 
+-- k = integer, n = integer
+
+-- These test cases will cover all possible inputs and will make sure
+-- nothing weird happens with certain inputs
+
+-- B
+
+-- Property function for all three different implementations of the
+-- power function
+
+prop_powers n k = (power n k) == (power1 n k) && (power n k) == (power2 n k)
+
+-- C
+-- Tuple list with all test cases from A, test_powers will test all cases
+testCases n k = [(0,0), (n,0), (0,k), (n,k)]
+test_powers n k = [prop_powers (fst x) (snd x) | x <- testCases n k]
+
+--D
+
+-- We do not allow negative numbers for the power functions,
+-- thus we need to take the absolute value for each n and k.
+prop_powers' n k = (power (abs n) (abs k)) == (power1 (abs n) (abs k))
+ && (power (abs n) (abs k)) == (power2 (abs n) (abs k))
+
