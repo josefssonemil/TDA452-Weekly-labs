9b4ac43e52cdbf0a9900dfc253cc3eeb0f4b37b2
96kevrud <kevinleerudnick@gmail.com>
2018-11-10 16:57:43 +0100

    Think I did A1 task, seemed a bit to easy. Typecheck is correct though

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -1,3 +1,7 @@
+module BlackJack where
+import Cards
+import RunGame
+
 -- A0
 -- hand2 can be interpretted to kinda 2_hearts + (jack_spades + empty)
 -- ... =  1 + size ( Add(card Jack Spades) empty
@@ -5,4 +9,7 @@
 -- = 2 + 0 = 2
 
 -- A1
+empty :: Hand
+empty = Empty
 
+-- A2
