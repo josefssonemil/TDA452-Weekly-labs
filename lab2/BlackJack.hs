df27575807427ea1a6c3b257a11a96f084fa96cb
96kevrud <kevinleerudnick@gmail.com>
2018-11-10 18:21:25 +0100

    Finished A2 task. Have done some testing, seems to work fine

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -13,3 +13,22 @@ empty :: Hand
 empty = Empty
 
 -- A2
+
+--Recurive thru the hand. Takes a card, finds its value and then continues
+--with the rest of the hand untill its empty.
+valueWithValueOfAce :: Integer -> Hand -> Integer
+valueWithValueOfAce aV Empty = 0
+valueWithValueOfAce aV (Add card hand) = (valueCard aV card) + 
+                       (valueWithValueOfAce aV hand) 
+
+-- Ace has AceValue here. 1 or 11
+valueRank :: Integer -> Rank -> Integer
+valueRank aV Jack = 10
+valueRank aV Queen = 10
+valueRank aV King = 10
+valueRank aV Ace = aV
+valueRank aV (Numeric x) = x
+
+--Give valueRank the rank of the card and the aceValue
+valueCard :: Integer -> Card -> Integer
+valueCard aceValue (Card {rank=r}) = (valueRank aceValue r)
