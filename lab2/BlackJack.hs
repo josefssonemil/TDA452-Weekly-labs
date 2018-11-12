e521792b49736bd057ca8b1be129ce9b1fa8d685
Emil Josefsson <emiljos@student.chalmers.se>
2018-11-12 15:15:53 +0100

    lab 2 finished

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -3,25 +3,32 @@ import Cards
 import RunGame
 
 -- A0
--- hand2 can be interpretted to kinda 2_hearts + (jack_spades + empty)
--- ... =  1 + size ( Add(card Jack Spades) empty
--- = 2 + size empty
--- = 2 + 0 = 2
+-- Hand2 consists of Hearts 2 and Spades Jack + empty , which gives
+-- 1 + Spades Jack + empty which gives 1 + 1 + 0 = 2
 
 -- A1
+-- Returns the empty hand
 empty :: Hand
 empty = Empty
 
 -- A2
+-- Takes a hand, compares it with different values of Ace (1 or 11).
+-- It computes the initial value, and then the initial value by checking
+-- if the initial value is larger than 21.
+value :: Hand -> Integer
+value hand = if valueWithValueOfAce 11 hand <= 21
+             then valueWithValueOfAce 11 hand
+             else valueWithValueOfAce 1 hand
 
---Recurive thru the hand. Takes a card, finds its value and then continues
---with the rest of the hand untill its empty.
+
+-- Checks the hand recursively by taking a card, finding its value and then
+-- continues with the hand until empty
 valueWithValueOfAce :: Integer -> Hand -> Integer
 valueWithValueOfAce aV Empty = 0
-valueWithValueOfAce aV (Add card hand) = (valueCard aV card) + 
-                       (valueWithValueOfAce aV hand) 
+valueWithValueOfAce aV (Add card hand) = (valueCard aV card) +
+                       (valueWithValueOfAce aV hand)
 
--- Ace has AceValue here. 1 or 11
+-- Returns the value of a rank
 valueRank :: Integer -> Rank -> Integer
 valueRank aV Jack = 10
 valueRank aV Queen = 10
@@ -29,6 +36,26 @@ valueRank aV King = 10
 valueRank aV Ace = aV
 valueRank aV (Numeric x) = x
 
---Give valueRank the rank of the card and the aceValue
+-- Returns the value of a card
 valueCard :: Integer -> Card -> Integer
-valueCard aceValue (Card {rank=r}) = (valueRank aceValue r)
+valueCard aceValue Card {rank=r} = valueRank aceValue r
+
+-- A3
+-- Checks if the game is over by comparing the value of the hand with 21
+gameOver :: Hand -> Bool
+gameOver hand = 21 < value hand
+
+-- A4
+
+-- Win conditions :
+-- handG closer to 21 than handB, then handG wins
+-- only handB bust, then handG wins
+-- otherwise handB wins
+-- We check this by finding the win conditions for the Guest, and if those
+-- are not fulfilled, the bank wins
+winner :: Hand -> Hand -> Player
+winner handG handB
+      | value handG <= 21 &&
+      21 - value handG < 21 - value handB     = Guest
+      | value handB > 21 && value handG <= 21 = Guest
+      | otherwise                             = Bank
