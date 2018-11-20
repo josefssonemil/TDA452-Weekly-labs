997b3a9080a1d6984f42a83428384e0546ab6a6f
96kevrud <kevinleerudnick@gmail.com>
2018-11-20 13:02:17 +0100

    B6 done. Just added the code from the assigment. Game works fine

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -179,3 +179,21 @@ prop_shuffle_sameCards g c h =
 -- makes sure that the size is preserved
 prop_size_shuffle :: StdGen -> Hand -> Bool
 prop_size_shuffle g hand = size hand == (size $ shuffle g hand)
+
+--b6 
+implementation = Interface
+  { iEmpty    = empty
+  , iFullDeck = fullDeck
+  , iValue    = value
+  , iGameOver = gameOver
+  , iWinner   = winner 
+  , iDraw     = draw
+  , iPlayBank = playBank
+  , iShuffle  = shuffle
+  }
+
+main :: IO ()
+main = runGame implementation
+
+
+
